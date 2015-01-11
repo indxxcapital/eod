abefc3388d81d1f2869a272c7767ca1b9a22905e
Amit Mahajan <amitmahajan86@gmail.com>
2015-01-11 12:42:11 +0530

    Revert file cleanup.

--- multicurrency2/revert_closing_process.php
+++ multicurrency2/revert_closing_process.php
@@ -2,139 +2,126 @@
 <?php
 include("dbconfig.php");
 
-if (DEBUG)
-{
-	define("date", '2014-08-27');
-}
-else
+define("date", '2014-08-27');
+if (!DEBUG)
 {
 	if ($_GET['date'])
 	{
-		define("date", $_GET['id']);
+		define("date", $_GET['date']);
 	}
 	else	
 	{
-		echo "Please define the revert date." . PHP_EOL;
+		printf("Please define the revert date.\n");
 		exit();
 	} 
 } 
+printf("Reverting all data till %s (inclusive).\n", date);
 
-/* Clean currency factor data */
-$query = "DELETE FROM `tbl_curr_prices` WHERE `date` = '" .date. "'";
-mysql_query($query);
-
-if ($err = mysql_errno())
-	echo "Currency factor clean query failed [error=" .$err. "]." . PHP_EOL;
-else
-	echo "Currency factor cleaned!" . PHP_EOL;
-
-/* Clean libor rate data */
-$query = "DELETE FROM `tbl_libor_prices` WHERE `date` = '" .date. "'";
-mysql_query($query);
+/* Clean currency factor table */
+mysql_query("DELETE FROM `tbl_curr_prices` WHERE `date` >= '" .date. "'");
 
 if ($err = mysql_errno())
-	echo "Libor rate clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Currency factor table clean query failed [error = %d]\n", $err);
 else
-	echo "Libor rate cleaned!" . PHP_EOL;
+	printf("\t+Currency factor table cleaned!\n");
 
-/* Clean cash index data */
-$query = "DELETE FROM `tbl_cash_prices` WHERE `date`  = '" .date. "'";
-mysql_query($query);
-if ($err = mysql_errno())
-	echo "Cash index clean query failed [error=" .$err. "]." . PHP_EOL;
-else
-	echo "Cash index cleaned!" . PHP_EOL;
+/* Clean libor rate table */
+mysql_query("DELETE FROM `tbl_libor_prices` WHERE `date` >= '" .date. "'");
 
-/* Clean security price data */
-$query = "DELETE FROM `tbl_prices_local_curr` WHERE `date`  = '" .date. "'";
-mysql_query($query);
 if ($err = mysql_errno())
-	echo "Price clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Libor rate table clean query failed [error = %d]\n", $err);
 else
-	echo "Price table cleaned!" . PHP_EOL;
+	printf("\t+Libor rate table cleaned!\n");
 
-/* Clean converted security price data */
-$query = "DELETE FROM `tbl_final_price` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+/* Clean cash index table */
+mysql_query("DELETE FROM `tbl_cash_prices` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Converted price clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Cash index table clean query failed [error = %d\n]", $err);
 else
-	echo "Converted price table cleaned!" . PHP_EOL;
+	printf("\t+Cash index table cleaned!\n");
+
+/* Clean security price table */
+mysql_query("DELETE FROM `tbl_prices_local_curr` WHERE `date`  >= '" .date. "'");
+if ($err = mysql_errno())
+	printf("\t-Security price table clean query failed [error = %d]\n", $err);
+else
+	printf("\t+Security price table cleaned!\n");
+
+/* Clean converted security price table */
+mysql_query("DELETE FROM `tbl_final_price` WHERE `date`  >= '" .date. "'");
+if ($err = mysql_errno())
+	printf("\t-Converted security price table clean query failed [error = %d]\n", $err);
+else
+	printf("\t+Converted security price table cleaned!\n");
 
 /* Clean converted security price data - upcoming */
-$query = "DELETE FROM `tbl_final_price_temp` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_final_price_temp` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Converted price[upcoming] clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Converted security price[upcoming] table clean query failed [error = %d]\n", $err);
 else
-	echo "Converted price[upcoming] table cleaned!" . PHP_EOL;
+	printf("\t+Converted security price[upcoming] table cleaned!\n");
 
 /* Clean calculated index values */
-$query = "DELETE FROM `tbl_indxx_value` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_indxx_value` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Index value table clean query failed [error = %d]\n", $err);
 else
-	echo "Index value table cleaned!" . PHP_EOL;
+	printf("\t+Index value table cleaned!\n");
 
 /* Clean calculated upcoming index values */
-$query = "DELETE FROM `tbl_indxx_value_temp` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_indxx_value_temp` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Upcoming index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Index value[upcoming] table clean query failed [error = %d]\n", $err);
 else
-	echo "Upcoming index value table cleaned!" . PHP_EOL;
+	printf("\t+Index value[upcoming] table cleaned!\n");
 
-/* TODO: Not cleaning the tbl_indxx_log and tbl_indxx_log_temp tables whle closing file generation process */
+/* 
+ * TODO: Not cleaning the tbl_indxx_log and tbl_indxx_log_temp tables 
+ * while closing file generation process. Check if this is needed.
+ */
 
 /* Clean cash index values */
-$query = "DELETE FROM `tbl_cash_indxx_value` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_cash_indxx_value` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Cash index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Cash indexes table clean query failed [error = %d]\n", $err);
 else
-	echo "Cash index value table cleaned!" . PHP_EOL;
+	printf("\t+Cash indexes table cleaned!\n");
 
 /* Clean upcoming cash index values */
-$query = "DELETE FROM `tbl_cash_indxx_value_temp` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_cash_indxx_value_temp` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "Cash index[upcoming] value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-Cash indexes[upcoming] table clean query failed [error = %d]\n", $err);
 else
-	echo "Cash index[upcoming] value table cleaned!" . PHP_EOL;
+	printf("\t+Cash indexes[upcoming] table cleaned!\n");
 
 /* Clean LSC index values */
-$query = "DELETE FROM `tbl_indxx_lsc_value` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_indxx_lsc_value` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "LSC index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-LSC index table clean query failed [error = %d]\n", $err);
 else
-	echo "LSC index value table cleaned!" . PHP_EOL;
+	printf("\t+LSC index table cleaned!\n");
 
 /* Clean CSI index values */
-$query = "DELETE FROM `tbl_indxx_cs_value` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+mysql_query("DELETE FROM `tbl_indxx_cs_value` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "CSI index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-CSI index table clean query failed [error = %d]\n", $err);
 else
-	echo "CSI index value table cleaned!" . PHP_EOL;
+	printf("\t+CSI index table cleaned!\n");
 
-/* Clean CSI index values */
-$query = "DELETE FROM `tbl_indxx_sl_value` WHERE `date`  = '" .date. "'";
-mysql_query($query);
+/* Clean SL index values */
+mysql_query("DELETE FROM `tbl_indxx_sl_value` WHERE `date`  >= '" .date. "'");
 if ($err = mysql_errno())
-	echo "SL index value clean query failed [error=" .$err. "]." . PHP_EOL;
+	printf("\t-SL index table clean query failed [error = %d]\n", $err);
 else
-	echo "SL index value table cleaned!" . PHP_EOL;
+	printf("\t+SL index table cleaned!\n");
 
 if (false)
 {
 	/* Clean weights table */
-	$query = "DELETE FROM `tbl_weights` WHERE `date`  = '" .date. "'";
-	mysql_query($query);
+	mysql_query("DELETE FROM `tbl_weights` WHERE `date`  >= '" .date. "'");
 	if ($err = mysql_errno())
-		echo "Security weights table value clean query failed [error=" .$err. "]." . PHP_EOL;
+		printf("\t-Security weights table clean query failed [error = %d]\n", $err);
 	else
-		echo "Security weights table value table cleaned!" . PHP_EOL;
+		printf("\t+Security weights table table cleaned!\n");
 }
 ?>
