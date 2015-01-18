7dcd86b2d3b40ffc3f1cf5da6be7f9d3d154c1f9
Amit Mahajan <amitmahajan86@gmail.com>
2015-01-18 15:02:13 +0530

    checkcavalue optimized.

--- icai2/classes/checkcavalue.class.php
+++ icai2/classes/checkcavalue.class.php
@@ -8,9 +8,11 @@ class Checkcavalue extends Application
 	
 	function index() 
 	{
-		/* TODO: Convert all getresult calls into mysql calls, paging isn;t needed */
-		/* TODO: This logic can be optimized more */
+		//$time = explode ( ' ', microtime () );
+		//$curr_time = $time [1] + $time [0];
 		
+		/* TODO: Convert all getresult calls into mysql calls, paging isn;t needed */
+
 		$datevalue2 = $this->_date;
 		
 		if($_GET['log_file'])
@@ -20,11 +22,7 @@ class Checkcavalue extends Application
 			define("DEBUG", $_GET['DEBUG']);
 		
 		$this->log_info(log_file, "CA checkcavalue process started");
-
-		$this->_title = $this->siteconfig->site_title;
-		$this->_meta_description = $this->siteconfig->default_meta_description;
-		$this->_meta_keywords = $this->siteconfig->default_meta_keyword;
-				
+		
 		$checkArray = array ();
 		$checkArray ['DVD_CASH'] 	= array('CP_NET_AMT','CP_GROSS_AMT', 'CP_DVD_CRNCY', 'CP_DVD_TYP');
 		$checkArray ['CHG_NAME'] 	= array('CP_OLD_NAME', 'CP_NEW_NAME');
@@ -33,56 +31,44 @@ class Checkcavalue extends Application
 		$checkArray ['DVD_STOCK'] 	= array('CP_AMT');
 		$checkArray ['STOCK_SPLT'] 	= array('CP_ADJ');
 		$checkArray ['RIGHTS_OFFER'] = array ('CP_RATIO', 'CP_ADJ', 'CP_PX', 'CP_CRNCY');
-
+		
 		$text = '';
 		
-		$final_array = array ();
-		
-		$indxxs = $this->db->getResult ( "select * from tbl_indxx  where status='1' and usersignoff='1' and dbusersignoff='1' and submitted='1' ", true );
-		
-		if (! empty ( $indxxs )) 
+		$query = "select distinct ind.id as ind_id, it.ticker, ca.identifier, ca.action_id, ca.id, ca.mnemonic, ca.company_name, ca.eff_date, ca.currency    
+				from tbl_indxx ind join tbl_holidays hds on hds.zone_id=ind.zone 
+				join tbl_indxx_ticker it on it.indxx_id=ind.id 
+				join tbl_ca ca on ca.identifier=it.ticker 
+				where ind.status='1' and ind.usersignoff='1' and ind.dbusersignoff='1' and ind.submitted='1' 
+				and hds.date!='" .$datevalue2. "' and eff_date='" .$datevalue2 ."'";
+		$res = mysql_query($query);
+		if (($err_code = mysql_errno()))
 		{
-			foreach ( $indxxs as $row ) 
+			$this->log_error(log_file, "Mysql query failed, error code " . $err_code . ". Exiting CA process.");
+			mail_exit(__FILE__, __LINE__);
+		}
+		
+		while (false != ($ca =mysql_fetch_assoc($res)))
+		{				
+			if (array_key_exists ( $ca ['mnemonic'], $checkArray ))
 			{
-				if ($this->checkHoliday ( $row ['zone'], $datevalue2 )) 
+				foreach ( $checkArray [$ca ['mnemonic']] as $fieldname )
 				{
-					$final_array [$row ['id']] = $row;
-					
-					$query = "SELECT  it.ticker  FROM `tbl_indxx_ticker` it where it.indxx_id='" . $row ['id'] . "'";
-					
-					$indxxprices = $this->db->getResult ( $query, true );
+					$ca_value_query = "Select id from tbl_ca_values where ca_id='" . $ca ['id'] . "'  and ca_action_id='" . $ca ['action_id'] . "' and field_name='" . $fieldname . "' ";
 
-					if (! empty ( $indxxprices )) 
-					{
-						foreach ( $indxxprices as $key => $indxxprice ) 
-						{
-							$ca_query = "select identifier, action_id, id, mnemonic, company_name, eff_date, currency from tbl_ca cat where  eff_date='" . $datevalue2 . "' and identifier='" . $indxxprice ['ticker'] . "' ";							
-							$cas = $this->db->getResult ( $ca_query, true );
-							
-							if (! empty ( $cas )) 
-							{
-								foreach ( $cas as $cakey => $ca ) 
-								{	
-									if (array_key_exists ( $ca ['mnemonic'], $checkArray )) 
-									{
-										foreach ( $checkArray [$ca ['mnemonic']] as $fieldname ) 
-										{
-											$ca_value_query = "Select id from tbl_ca_values where ca_id='" . $ca ['id'] . "'  and ca_action_id='" . $ca ['action_id'] . "' and field_name='" . $fieldname . "' ";
-
-											$ca_values = $this->db->getResult ( $ca_value_query );
-											if (count ( $ca_values ) <= 0)
-												$text .= $ca ['company_name'] . "(" . $ca ['identifier'] . ")=>" . $_SESSION ['variable'] [$ca ['mnemonic']] . "=>" . $_SESSION ['variable'] [$fieldname] . "=>" . $fieldname . "<br>";
-										}
-									}
-								}
-							}
-							$indxxprices [$key] ['ca'] = $cas;
-						}
-					}
+					$ca_values = $this->db->getResult ( $ca_value_query );
+					if (count ( $ca_values ) <= 0)
+						$text .= $ca ['company_name'] . "(" . $ca ['identifier'] . ")=>" . $_SESSION ['variable'] [$ca ['mnemonic']] . "=>" . $_SESSION ['variable'] [$fieldname] . "=>" . $fieldname . "<br>";
 				}
 			}
 		}
+		mysql_free_result($res);
 
+		//echo "text = " .$text. "<br>";
+		//$time = explode ( ' ', microtime () );
+		//$end_time = $time [1] + $time [0];
+		//echo "Time taken = " .round(($end_time - $curr_time), 4) . "<br>";
+		//return;
+		
 		if ($text != '' && $text) 
 		{
 			$useremails = $this->db->getResult ( "select email from tbl_ca_user where 1=1", true );
