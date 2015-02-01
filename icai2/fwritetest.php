dd3f7b7dfa35e0045af1aa97005a0cfdff4213a9
Amit Mahajan <amitmahajan86@gmail.com>
2015-02-01 13:00:30 +0530

    Delete useless files.

--- icai2/fwritetest.php
+++
@@ -1,5 +0,0 @@
-<?php
-$myfile = fopen("templates_c/%%AB^AB1^AB11093F%%login-template.tpl.php", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("templates_c/%%AB^AB1^AB11093F%%login-template.tpl.php"));
-fclose($myfile);
-?>
